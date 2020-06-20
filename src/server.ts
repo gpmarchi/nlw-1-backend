4b7ee208236f39e30e108fb676fb93e02eae225b
gpmarchi <42723081+gpmarchi@users.noreply.github.com>
2020-06-20 14:35:43 -0300

    apply prettier formatting

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
-import express from "express";
-import cors from "cors";
-import path from "path";
-import routes from "./routes";
+import express from 'express';
+import cors from 'cors';
+import path from 'path';
+import routes from './routes';
 
 const app = express();
 
@@ -9,6 +9,6 @@ app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-app.use("/uploads", express.static(path.resolve(__dirname, "..", "uploads")));
+app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
 
 app.listen(3333);
